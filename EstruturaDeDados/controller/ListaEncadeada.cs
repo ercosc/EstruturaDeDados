c5c1b3406af4c7b53d01d6999c312bf493ac6a40
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-07 14:18:06 -0300

    construtor da lista

--- EstruturaDeDados/controller/ListaEncadeada.cs
+++ EstruturaDeDados/controller/ListaEncadeada.cs
@@ -13,8 +13,22 @@ namespace EstruturaDeDados.controller
         public ListaEncadeada()
         {
             inicio = new Node();
+            tamanho = 0;
+            fim = inicio;
         }
-        
+        public void AddInicio(int x)
+        {
+
+        }
+        public void AddFim(int x)
+        {
+
+        }
+        public string listar()
+        {
+            string resultado = "";
+            return resultado;
+        }
 
     }
 }
